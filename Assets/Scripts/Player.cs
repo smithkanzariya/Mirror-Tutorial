b570a15b34a7110b4c3a09c878d89609088d424c
smithkanzariya <smithkanzariya@gmail.com>
2022-11-28 17:40:22 +0530

    Hello world -multiplayer edition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class Player : NetworkBehaviour
+{
+    float moveHorizontal;
+    float moveVertical;
+    Vector3 move;
+    void HandleMovements()
+    {
+        if(isLocalPlayer)
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            moveVertical = Input.GetAxis("Vertical");
+            move = new Vector3(-moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + move;
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovements();
+    }
+}
